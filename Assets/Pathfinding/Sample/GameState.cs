baf840948b786a1abf4bfed886946e82741640e6
asilveira0001 <unknown-user@fe80::d9b9:e89f:87c:4771%6>
2015-01-24 02:50:41 -0500

    grr

--- Assets/Pathfinding/Sample/GameState.cs
+++ Assets/Pathfinding/Sample/GameState.cs
@@ -5,12 +5,21 @@ using System.Collections;
 
 public class GameState : MonoBehaviour {
 
-	const float secondsPerClick;
-	int currentClick;
+	// Constant for the number of seconds that elapse before a "Click" happens.
+	const float secondsPerClick = 5.0f;
+
+
+	private float startTime = Time.time;
+	
+
+	// Current game "Click" starts at 0 and can go back and forth in time. ie -1, 0, 1 
+	// (This needs to be changed to an atribute)
+	int currentClick = 0;
 
 
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
