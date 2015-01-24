c2a00bc1ddeb20811a294b2ae9e2f102b1c93163
asilveira0001 <asilveira0001@BLN-CB201-B6.kctcsacc.ds.kctcs.edu>
2015-01-24 04:56:37 -0500

    um

--- Assets/Pathfinding/Sample/GameState.cs
+++ Assets/Pathfinding/Sample/GameState.cs
@@ -3,38 +3,42 @@ using System.Collections;
 
 
 
-public class GameState : MonoBehaviour {
+public class GameStateController : MonoBehaviour 
+{
+
+	public enum GameState { Setup, Excecution };
 
 	// Constant for the number of seconds that elapse before a "Click" happens.
 	public const float secondsPerClick = 5.0f;
-
-
-	// Variables startTime to keep track of the time when game starts.
-	private float startTime;
-	private float currentTime;
-
 	
+	private float lastClickTime;
+	
+	// Current "Click", Denotes number of discrete time periods before or after start of game.
+	private int currentClick = 0;
+	public int CurrentClick { get { return currentClick; } }
 
-	// Current game "Click" starts at 0 and can go back and forth in time. ie -1, 0, 1 
-	// (This needs to be changed to an property)
-	public int currentClick = 0;
 
+	private GameState currentGameState = GameState.Setup;
+	public GameState CurrentGameState { get { return currentGameState; } }
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 
-		startTime = Time.time;
-		currentTime = startTime;
+		lastClickTime = Time.time;
 
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
-	
-		// If "secondsPerClick" seconds have passed...
-		if ( (Time.time - currentTime) > secondsPerClick )
+	void Update () 
+	{
+		// Keep track of what curent "Click" is once execution has begun.
+		if ((Time.time - lastClickTime) > secondsPerClick) 
+		{
 			
-			currentClick += 1;            // "Click" game forward by 1
-			currentTime = Time.time;	  // Set currentTime to the current system time.
+			currentClick += 1;            
+			lastClickTime = Time.time;	  
+		}
 	}
 }
